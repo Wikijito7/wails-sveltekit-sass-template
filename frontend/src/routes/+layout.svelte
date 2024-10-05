8ffab95d37a9c7c930bb1dcbd9ded58b593bafe8
wikijito7 <wikyfg@gmail.com>
2024-10-05 13:07:39 +0200

    First commit

---
+++ frontend/src/routes/+layout.svelte
@@ -0,0 +1,18 @@
+<script lang="js">
+    import Header from '../components/Header.svelte';
+import '../scss/app.scss';
+</script>
+
+<main>
+    <Header />
+    <div class="content">
+        <slot />
+    </div>
+</main>
+
+<style lang="scss">
+    content {
+        display: flex;
+        flex-direction: column;
+    }
+</style>
---
+++ frontend/src/routes/+page.svelte
@@ -0,0 +1,11 @@
+<script>
+
+</script>
+
+<main>
+    <p>Main</p>
+</main>
+
+<style lang="scss">
+
+</style>
